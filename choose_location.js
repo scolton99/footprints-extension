251f5d2dffd96e2cde690ea504fe3fd0bf400c44
Spencer Colton <me@scolton.tech>
2018-02-03 13:01:57 -0600

    Test Subnet-Based Location Choosing
    Use external code to retrieve internal client IP; compare this IP to NUSA-listed subnets for TSC-Sherman and TSC-Library; if match, select location based on IP, otherwise prompt for locaiton.

--- choose_location.js
+++ choose_location.js
@@ -1,14 +1,65 @@
 // ask user where they are, must be either "library" or "sherman"
-var user_location;
-var msg = "Where are you working? Enter 'sherman' or 'library'."
-while (true) {
-    user_location = window.prompt(msg);
-    if (user_location === "sherman" || user_location === "library") {
-        break;
-    } else {
-        msg = "Invalid location supplied. Enter 'sherman' or 'library'."
+var user_location = null;
+var msg = "Where are you working? Enter 'sherman' or 'library'.";
+
+function select(ips) {
+    for (var ip in ips) {
+        if (!ips.hasOwnProperty(ip))
+            continue;
+
+        if (ip.startsWith("129.105.22")) {
+            user_location = "library";
+            break;
+        } else if (ip.startsWith("129.105.188")) {
+            user_location = "sherman";
+            break;
+        }
+    }
+
+    if (user_location == null) {
+        while (true) {
+            user_location = window.prompt(msg);
+            if (user_location === "sherman" || user_location === "library") {
+                break;
+            } else {
+                msg = "Invalid location supplied. Enter 'sherman' or 'library'."
+            }
+        }
     }
 }
+
+// Taken from https://stackoverflow.com/questions/18572365/get-local-ip-of-a-device-in-chrome-extension
+function getLocalIPs(callback) {
+    var ips = [];
+
+    var RTCPeerConnection = window.RTCPeerConnection ||
+        window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+
+    var pc = new RTCPeerConnection({
+        // Don't specify any stun/turn servers, otherwise you will
+        // also find your public IP addresses.
+        iceServers: []
+    });
+    // Add a media line, this is needed to activate candidate gathering.
+    pc.createDataChannel('');
+
+    // onicecandidate is triggered whenever a candidate has been found.
+    pc.onicecandidate = function(e) {
+        if (!e.candidate) { // Candidate gathering completed.
+            pc.close();
+            callback(ips);
+            return;
+        }
+        var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
+        if (ips.indexOf(ip) == -1) // avoid duplicate entries (tcp/udp)
+            ips.push(ip);
+    };
+    pc.createOffer(function(sdp) {
+        pc.setLocalDescription(sdp);
+    }, function onerror() {});
+}
+
+getLocalIPs(select);
 
 // save to chrome storage -- note this overwrites previous value,
 // which handles the user working in different locations
