5697ab0baf004fa0b3fe1ff25fb3e237f33c40ae
Tushar Chandra <TusharChandra2018@u.northwestern.edu>
2018-04-23 20:20:10 -0500

    Fix bug with pageload

--- disable_conweb_banner.js
+++ disable_conweb_banner.js
@@ -1,3 +1,5 @@
 // find the fp banner, set to display none
-var banner = document.getElementById("fpExtensionDiv");
-banner.style.display = 'none';
+window.onload = function() {
+    var banner = document.getElementById("fpExtensionDiv");
+    banner.style.display = 'none';
+}
