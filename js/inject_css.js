a5e9f04725d7ed8b771e89479425508c32253e6e
Spencer Colton <me@scolton.tech>
2018-11-26 08:45:05 -0600

    Fix Attachment Support on Non-Pretty FP

--- js/inject_css.js
+++ js/inject_css.js
@@ -9,7 +9,7 @@ function injectStyles(url) {
       setTimeout(injectStyles.bind(null, url), 10);
       return;
     }
-    
+
     var elem = document.createElement('link');
     elem.rel = 'stylesheet';
     elem.setAttribute('href', url);
@@ -21,6 +21,7 @@ function injectAll() {
   injectStyles(chrome.extension.getURL('vendor/fontawesome/css/all.min.css'));
   injectStyles(chrome.extension.getURL('css/fp-material.css'));
   injectStyles(chrome.extension.getURL('css/fixify.css'));
+  injectStyles(chrome.extension.getURL('css/attachments.css'));
 }
 
 injectAll();
