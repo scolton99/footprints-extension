7945223e35d80e19c259124ad1b7084f2d988f00
Spencer Colton <me@scolton.tech>
2019-07-24 10:38:33 -0500

    Ticket quick search opens in new tab instead of window

--- js/tabs.js
+++ js/tabs.js
@@ -29,6 +29,77 @@ const openInTabs = () => {
         var winName = 'Create' + randomize;
         window.open(url, winName); 
     };
+
+    window.submitQuickSearch = () => {
+        document.quickSearch.SEARCHS.value = document.quickSearch.SEARCHS.value.replace(/^\s+|\s+$/g, '');
+        var searchValue = document.quickSearch.SEARCHS.value;
+
+        if (!searchValue || !searchValue.match(/\S/))
+           return;
+
+        if (document.quickSearch.WSEARCH.value == 'NAMP') {
+           showNumaraAmpSearchWindow(searchValue);
+           return;
+        }
+
+        // detect numeric input
+        // one number, no delimiters. Just load details page. No form submit.
+        if (searchValue.match(/^\d+$/)) {
+
+           var number = parseInt(searchValue, 10);
+           if (number < 1)
+              return alert('All Issue numbers must be positive integers.');
+
+            const MRP = /(?:^|&|\?)MRP=(.*?)(?:&|$)/g.exec(detailsBaseURL)[1];
+            const user = /(?:^|&|\?)USER=(.*?)(?:&|$)/g.exec(detailsBaseURL)[1];
+            const projectid = document.getElementsByName("PROJECTID")[0].value;
+
+
+           var url  = `/MRcgi/MRTicketPage.pl?USER=${user}&PROJECTID=${projectid}&MRP=${MRP}&MAXMININC=&MAJOR_MODE=DETAILS&RUNNING_IN_POPUP=1`;
+           url += '&MR=' + number;
+
+           window.open(url, 'details' + number);
+
+           return;
+        }
+        else if (searchValue.match(/^(\d+,*\s*)+$/)) {
+
+           // in case spaces used as delimeter switch space to comma
+           searchValue = searchValue.replace(/,?\s+/g, ',');
+           var nums = searchValue.split(',');
+         
+           // eliminate nulls from the results of the split
+           var goodNums = new Array();
+           for (var i = 0; i < nums.length; i++)
+              if (nums[i] > 0)
+                 goodNums.push(nums[i]);
+           searchValue = goodNums.join(',');      
+
+           document.quickSearch.WSEARCH.value = 'NUM';
+           appendHidden(document.quickSearch, 'MR', searchValue);
+        }
+        else if (document.quickSearch.WSEARCH.value == 'ASSIGNEE') {
+           appendHidden(document.quickSearch, 'ASSIGNEDTO', searchValue );
+        }
+        else if (document.quickSearch.WSEARCH.value == 'TITLE') {
+           appendHidden(document.quickSearch, 'TITLE', searchValue);
+        }
+        else if (document.quickSearch.WSEARCH.value == 'KEYWORD') {
+           appendHidden(document.quickSearch, 'KEYWORDPHRASE', searchValue);
+        }
+        else if (document.quickSearch.WSEARCH.value == 'SLB_CUSTOM_ISSUE_NUMBER') {
+           // we are searching on SLB_CUSTOM_ISSUE_NUMBER and it must be an int
+           if (isNaN(searchValue))
+              return alert("Please enter a positive integer number");
+           // further treatment of the search string will occur after form is submitted
+        }
+        else if (document.quickSearch.WSEARCH.value == 'CUSTOM') {
+           null;
+           // further treatment of the search string will occur after form is submitted
+        }
+
+        document.quickSearch.submit();
+     };
 };
 
 openInTabs();
