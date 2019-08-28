a179e6e36881b6c4443339ae617d74897840e33e
Spencer Colton <me@scolton.tech>
2019-08-28 15:03:21 -0500

    Fixed tabs...again

--- js/inject_tabs.js
+++ js/inject_tabs.js
@@ -1,9 +1,11 @@
 chrome.storage.sync.get({
     tabs: true
 }, items => {
-    if (items.tabs) {
-        const tabsScript = document.createElement("script");
-        tabsScript.setAttribute("src", chrome.extension.getURL('/js/tabs.js'));
-        document.documentElement.appendChild(tabsScript);
-    }
+    window.addEventListener("load", () => {
+        if (items.tabs) {
+            const tabsScript = document.createElement("script");
+            tabsScript.setAttribute("src", chrome.extension.getURL('/js/tabs.js'));
+            document.documentElement.appendChild(tabsScript);
+        }
+    });
 });
