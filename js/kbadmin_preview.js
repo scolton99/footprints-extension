b958e11f2f0fd8449a472db2d481d652359f46af
Spencer Colton <me@scolton.tech>
2020-03-25 17:18:57 -0500

    js/kbadmin_preview.js: made KBAdmin previews work properly with infobox, tabs, and toggles
    Manually execute JS loaded via XHR to run scripts necessary to initialize tabs and toggles on ISPs and Team Pages

---
+++ js/kbadmin_preview.js
@@ -0,0 +1,23 @@
+const check_preview = () => {
+  const preview = document.getElementById("preview");
+
+  if (!preview || preview.textContent === "") {
+    window.setTimeout(check_preview, 50);
+    return;
+  }
+
+  preview.style.margin = "auto";
+  preview.style.maxWidth = "1250px";
+
+  preview.querySelectorAll("script").forEach(x => {
+    if (x.textContent === "") {
+      const sct = document.createElement("script");
+      sct.src = x.src;
+      document.body.appendChild(sct);
+    } else {
+      eval(x.textContent);
+    }
+  });
+};
+
+check_preview();
