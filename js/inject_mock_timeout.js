b8fb96653b2201a8a190376d36f507319dd8a0a8
Mateusz Ryczek <skyfused@gmail.com>
2020-06-30 21:31:54 -0500

    remove dead code

--- js/inject_mock_timeout.js
+++
@@ -1,3 +0,0 @@
-const script = document.createElement("script");
-script.setAttribute("src", chrome.extension.getURL('/js/mock_timeout.js'));
-document.documentElement.appendChild(script);
--- js/mock_timeout.js
+++
@@ -1,8 +0,0 @@
-const oldTimeout = window.setTimeout;
-window.setTimeout = function(callback, timeout) {
-    if (timeout === 900000 || timeout === 1800000 || timeout === 3600000) {
-        return;
-    }
-    
-    oldTimeout(callback, timeout);
-};
